0da9d4ed8cf2468704a329182d4802486cdb45f3
Kiyo <kyoponayanokoji@gmail.com>
2024-09-12 22:43:00 +0800

    Some Animation and Visual Changes

---
+++ scripts/scroll-reveal.js
@@ -0,0 +1,19 @@
+ScrollReveal({
+  //reset: true,
+  distance: '60px',
+  duration: 2000,
+  delay: 400
+  });
+  
+ScrollReveal().reveal('.nav-links', {delay: 300, origin: 'top', interval: 300});
+ScrollReveal().reveal('.header-campus-col', {delay: 300, origin: 'right', interval: 300});
+ScrollReveal().reveal('.text-box', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.header-text', {delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('.display-col', {delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('.description', {delay: 300, origin: 'right'});
+ScrollReveal().reveal('.campus-header', {delay: 300, origin: 'left'});
+ScrollReveal().reveal('.campus-col', {delay: 500, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.facilities', {delay: 600, origin: 'right'});
+ScrollReveal().reveal('.facilities-col', {delay: 500, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.testimonials-header', {delay: 600, origin: 'left'});
+ScrollReveal().reveal('.testimonial-col', {delay: 600, origin: 'right', interval: 300});
